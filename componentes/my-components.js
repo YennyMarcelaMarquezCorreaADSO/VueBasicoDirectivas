91ae0d56eb97ef04da0d0f36680fc175ea57d928
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2023-03-14 17:19:18 -0500

    feat: :sparkles: Directivas v-if, v-else, v-else-if
    Desarrollo de ejemplo el cual contiene las directivas v-if, v-else, v-else-if

--- componentes/my-components.js
+++ componentes/my-components.js
@@ -1,7 +1,11 @@
 export default{
     data(){
         return {
-            centinela: false 
+            mensaje1: `<em>Acotar</em>`,
+            mensaje2: `<mark>Trono de cristal</mark>`,
+            mensaje3: `<u>Crescent city</u>`,
+            mensaje4: `NO ES NINGUN LIBRO`,
+            opcion: null, 
         }
     }
 }
