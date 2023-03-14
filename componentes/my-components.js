cc5f5afdaab58c816e690f7c3ac0b8eea95181fd
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2023-03-14 17:13:31 -0500

    feat: :sparkles: Directiva v-model
    Desarrollo de ejemplo directiva v-model

--- componentes/my-components.js
+++ componentes/my-components.js
@@ -1,9 +1,7 @@
 export default{
     data(){
         return {
-            basico: `<strong style="color:green;">Acotar</strong>`,
-            intermedio: `<strong style="color:blue;">Trono de cristal</strong>`,
-            avanzado:`<strong style="color:red;">Crescent city</strong>`,
+            mensaje:"lorem",
         }
     }
 }
