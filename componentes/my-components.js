a7c3f64dd5d0f2e5d2a55bcaa75f92a6bd278898
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2023-03-14 17:21:25 -0500

    feat: :sparkles: Directiva v-for
    Desarrollo de ejemplo de la directiva v-for

--- componentes/my-components.js
+++ componentes/my-components.js
@@ -1,11 +1,11 @@
 export default{
     data(){
         return {
-            mensaje1: `<em>Acotar</em>`,
-            mensaje2: `<mark>Trono de cristal</mark>`,
-            mensaje3: `<u>Crescent city</u>`,
-            mensaje4: `NO ES NINGUN LIBRO`,
-            opcion: null, 
+            lista: [
+                "Acotar",
+                "Trono de cristal",
+                "Crescent city"
+            ] 
         }
     }
 }
